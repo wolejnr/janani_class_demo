314c0dfe83d102a626e02f059eb75dd1e1493b24
Adewole Adewumi <wolejr@gmail.com>
2025-02-15 09:20:44 -0500

    Done with Week6

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -9,6 +9,10 @@ class SecondPage extends StatefulWidget {
 }
 
 class _SecondPageState extends State<SecondPage> {
+  var words = <String>["Item 1", "Item 2"];
+
+  final TextEditingController _input = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +22,73 @@ class _SecondPageState extends State<SecondPage> {
       ),
       body: Center(
         child: Column(
-          children: [Text("Welcome to the Second Page " + DataRepository.loginName)],
+          children: [
+            Text("Welcome to the Second Page " + DataRepository.loginName),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _input,
+                    decoration: InputDecoration(hintText: "Type here"),
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if (_input.value.text.isNotEmpty) {
+                        setState(() {
+                          words.add(_input.value.text);
+                          _input.text = "";
+                        });
+                      } else {
+                        const snackBar =
+                            SnackBar(content: Text("Please input something"));
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      }
+                    },
+                    child: Text("Add Item"))
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: words.length,
+                  itemBuilder: (context, rowNum) {
+                    return GestureDetector(
+                      onLongPress: () {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Text("Delete?"),
+                                content: Text(
+                                    "Are you sure you want to delete the row?"),
+                                actions: [
+                                  ElevatedButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          words.removeAt(rowNum);
+                                          Navigator.pop(context);
+                                        });
+                                      },
+                                      child: Text("Yes")),
+                                  ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      }, child: Text("No"))
+                                ],
+                              );
+                            });
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text("Row Number: $rowNum"),
+                          Text(words[rowNum])
+                        ],
+                      ),
+                    );
+                  }),
+            )
+          ],
         ),
       ),
     );
